0bd84b27983814e349db17c52f8fb87a8a884f04
Dan Brendza <dan.brendza01@student.csulb.edu>
2026-03-17 18:44:46 -0700

    ex3

---
+++ js/ex3-4.js
@@ -0,0 +1,58 @@
+//Declare studentList Array
+const studentList = [
+  {
+    firstName: "Allan",
+    lastName: "Able",
+    scores: [95, 85, 92, 98]
+  },
+  {
+    firstName: "Amy",
+    lastName: "Alexander",
+    scores: [80, 88, 100]
+  },
+  {
+    firstName: "Betty",
+    lastName: "Barns",
+    scores: [70, 80, 90, 100]
+  },
+  {
+    firstName: "Bob",
+    lastName: "Bones",
+    scores: [75, 85, 95, 85]
+  },
+  {
+    firstName: "Cindy",
+    lastName: "Chase",
+    scores: [95, 90, 92, 98]
+  },
+  {
+    firstName: "Charles",
+    lastName: "Chips",
+    scores: [88, 99, 90]
+  },
+];
+
+// Compute min using reduce
+const getMin = scores =>
+  scores.reduce((min, s) => (s < min ? s : min), scores[0]);
+
+// Compute max using reduce
+const getMax = scores =>
+  scores.reduce((max, s) => (s > max ? s : max), scores[0]);
+
+// Compute average using reduce
+const getAvg = scores =>
+  scores.reduce((sum, s) => sum + s, 0) / scores.length;
+
+const cLastNameResults = studentList
+  .filter(s => s.lastName.startsWith("C")) // Only last names starting with C
+  .map(s => ({
+    firstName: s.firstName,
+    lastName: s.lastName,
+    minScore: getMin(s.scores),
+    maxScore: getMax(s.scores),
+    avgScore: getAvg(s.scores)
+  }));
+
+//Output
+console.log(cLastNameResults);
